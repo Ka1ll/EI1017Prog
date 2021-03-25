c6331da9e0de7789642ebfce301dd17ca45686ae
Ka1ll <123a789456*>
2021-03-25 19:38:21 +0100

    bueno

--- src/main/java/persona.java
+++ src/main/java/persona.java
@@ -1,5 +1,26 @@
+import java.util.List;
+
 public class persona {
-    //echo "Hola bon dia"
-    //hola
-    //hola2
+       String nombre;
+       String correo;
+       List<String> ListaTareas;
+
+
+    public persona(String nombre, String correo, List<String> listaTareas) {
+        this.nombre = nombre;
+        this.correo = correo;
+        ListaTareas = listaTareas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public List<String> getListaTareas() {
+        return ListaTareas;
+    }
 }
